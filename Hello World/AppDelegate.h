940d7fac25996212834e8615e491dc2778adf8aa
Amitai Schlair <aschlair@pillartechnology.com>
2015-03-09 19:29:21 -0400

    Initial Commit

---
+++ Hello World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Amitai Schlair on 2015-03-09.
+//  Copyright (c) 2015 Implemications LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Amitai Schlair on 2015-03-09.
+//  Copyright (c) 2015 Implemications LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
