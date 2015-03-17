013c042fb1b11a69bafa94a28829e3c66f491bd9
Amitai Schlair <aschlair@pillartechnology.com>
2015-03-17 18:29:52 -0400

    Lower deployment target to 8.1 for my iPhone

--- Hello WorldTests/Hello_WorldTests.m
+++ Hello WorldTests/Hello_WorldTests.m
@@ -10,31 +10,24 @@
 #import <XCTest/XCTest.h>
 
 @interface Hello_WorldTests : XCTestCase
-
 @end
 
 @implementation Hello_WorldTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+//- (void)setUp {
+//    [super setUp];
+//}
+//
+//- (void)tearDown {
+//    [super tearDown];
+//}
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
+- (void)testExamplePass {
     XCTAssert(YES, @"Pass");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testExampleFail {
+    XCTFail(@"Nope Nope Nope");
 }
 
 @end
